407f90691a67706731feeb7c4575b3822668cc34
NaPole On <kr0b3ck@gmail.com>
2026-04-05 20:25:55 +0300

    Update core.js

--- core/core.js
+++ core/core.js
@@ -1392,7 +1392,7 @@
             }
             
             const heartSvg = `
-                <svg class="heartSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" width="16" height="16">
+                <svg class="heartSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
                     <path class="heart" fill="#ABB9C2" d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5 2,5.41 4.42,3 7.5,3c1.74,0 3.41.81 4.5,2.08C13.09,3.81 14.76,3 16.5,3 19.58,3 22,5.41 22,8.5c0,3.77-3.4,6.86-8.55,11.54L12,21.35Z"/>
                 </svg>
             `;
